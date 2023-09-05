9f78975992636d77f47cf0f42f93a0cb740e7b71
yo02741 <a0922335447@gmail.com>
2023-09-05 09:03:19 +0800

    feat: update client project, vue to react

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,14 +1,20 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-recommended',
+  env: { browser: true, es2020: true },
+  extends: [
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
 }
