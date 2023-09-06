911fc66ff92f1fe9dcd00cf91a3ce38c14a5f783
yo02741 <a0922335447@gmail.com>
2023-09-06 09:10:05 +0800

    feat: App file update, home page prototype refactor: remove unuse

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,52 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+// function App() {
+//   const [count, setCount] = useState(0);
 
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank" rel="noreferrer">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import GhostPicture from "./GhostPicture";
+
+const App = () => {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>
+          <span>PTT</span>
+          <span>Marvel</span>
+          <span>Land</span>
+        </h1>
+        {/* <LoginThroughGoogle /> */}
+        <button>Login Later</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <GhostPicture />
     </>
   );
-}
+};
 
 export default App;
---
+++ client/src/GhostPicture.jsx
@@ -0,0 +1,7 @@
+import reactLogo from "@/assets/react.svg";
+
+function GhostPicture() {
+  return <img src={reactLogo} alt="" />;
+}
+
+export default GhostPicture;
