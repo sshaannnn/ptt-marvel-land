858d5eda539b491057295e2a88a3c5d3ab3d06f3
yo02741 <a0922335447@gmail.com>
2023-09-10 23:59:55 +0800

    try some react syntax

--- client/src/ArticleDetailPage.jsx
+++ client/src/ArticleDetailPage.jsx
@@ -1,12 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams, useOutletContext } from "react-router-dom";
 
 export default function ArticleDetailPage() {
   const { articleId } = useParams();
+  const props = useOutletContext(); // just try props
   return (
     <>
       <hr />
       <h2>ArticleDetailPage </h2>
       <h3>Article id: {articleId}</h3>
+      <span>author: {props.author}</span>
     </>
   );
 }
--- client/src/ArticlesListPage.jsx
+++ client/src/ArticlesListPage.jsx
@@ -1,30 +1,39 @@
 import { Link, Outlet } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+// just try child component, make sure 
+// curstom component begin with an uppercase letter
+const NotFound = () => {
+  return <p>你搞事吧！</p>
+}
 
 export default function ArticlesListPage() {
+  const articleListDict = {
+    "1": { title: "文章1", author: "小明" },
+    "2": { title: "文章2", author: "小黃" },
+    "3": { title: "文章3", author: "小王" },
+    "4": { title: "文章4", author: "小美" },
+    "5": { title: "文章5", author: "小智" },
+  }
+  const { articleId } = useParams();
   return (
     <>
       <Link to={"/"}>back to home</Link>
       <h2>ArticlesListPage</h2>
       <ul>
-        <li>
-          <Link to={`1111111111111`}>1111111111111</Link>
-        </li>
-        <li>
-          <Link to={`222222222222`}>222222222222</Link>
-        </li>
-        <li>
-          <Link to={`333333333333`}>333333333333</Link>
-        </li>
-        <li>
-          <Link to={`444444444444`}>444444444444</Link>
-        </li>
-        <li>
-          <Link to={`555555555555`}>555555555555</Link>
-        </li>
+        {/* just try rendering lists & handling style  */}
+        {Object.keys(articleListDict).map((articleId) => (
+          <li key={articleId} style={{ color: (articleId % 2) > 0 ? 'red': 'blue' }}>
+            <Link to={articleId}>{articleListDict[articleId].title}</Link>
+          </li>
+        ))}
       </ul>
 
       <div id="article">
-        <Outlet />
+        {/* just try props */}
+        { articleListDict[articleId] ? <Outlet context={articleListDict[articleId]} /> : `找不到文章 id: ${articleId}` } 
+        {/* just try conditional rendering and logical && syntax: */}
+        { !articleListDict[articleId] && <NotFound /> }
       </div>
     </>
   );
